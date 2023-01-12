cbc52a4824f11f6af3150cd75005700bea6b1b0b
Eszter Digruber <digruber.eszter@verebelyszki.hu>
2023-01-12 14:01:45 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -3,9 +3,14 @@
     class Program
     {
         static List<int> Szam_List = new List<int>();
+        static Random rnd = new Random();
         static void Main(string[] args)
         {
             //Listák
+            for (int i = 0; i < 200; i++)
+            {
+                Szam_List.Add(rnd.Next(1, 51));
+            }
         }
     }
 }
