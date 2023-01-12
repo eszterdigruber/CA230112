14e2721984bece3bd36201e104c32ff3de6eea64
digruber.eszter <digruber.eszter@117-02>
2023-01-12 14:00:24 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static List<int> Szam_List = new List<int>();
+        static void Main(string[] args)
+        {
+            //Listák
+        }
+    }
+}
