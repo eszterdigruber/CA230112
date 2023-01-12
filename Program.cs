d36d0a7e44ec64be939d8a216e6ea6ad061c5d47
Eszter Digruber <digruber.eszter@verebelyszki.hu>
2023-01-12 14:12:03 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -7,10 +7,27 @@
         static void Main(string[] args)
         {
             //Listák
+            //Lista random számokkal való feltöltése
             for (int i = 0; i < 200; i++)
             {
                 Szam_List.Add(rnd.Next(1, 51));
             }
+            //Kiiratás
+            for (int i = 0; i < Szam_List.Count; i++)
+            {
+                if (i % 20 == 0) 
+                { Console.Write("\n"); }
+                Console.Write($"{Szam_List[i]:00}, ");
+            }
+            //Fajl-ba kiiratni, txt, csv UTF 8-as kódolású ami ismeri az ékezetes betűket
+            var sw = new StreamWriter(@"szamlista.txt", false, System.Text.Encoding.UTF8);
+            for (int i = 0; i < Szam_List.Count; i++)
+            {
+                if (i % 20 == 0)
+                { sw.Write("\n"); }
+                sw.Write($"{Szam_List[i]:00}, ");
+            }
+            sw.Close();
         }
     }
 }
